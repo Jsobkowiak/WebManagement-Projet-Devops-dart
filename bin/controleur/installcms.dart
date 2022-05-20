a47b03805b02c4a19e05734780d7dddaed074953
Jonathan <jonathan.sobkowiak62@gmail.com>
2022-05-20 11:35:49 +0200

    Version

--- bin/controleur/installcms.dart
+++ bin/controleur/installcms.dart
@@ -4,7 +4,7 @@ class Installcms {
   static Future<void> installWordpress() async {
     String install =
         "cd /var/www/html/ && wget https://fr.wordpress.org/latest-fr_FR.zip";
-
+    String unzip = "unzip latest-fr_FR.zip";
     ProcessResult p1 = await Process.run('bash', ['-c', install]);
     print(p1.stdout);
     print(p1.stderr);
